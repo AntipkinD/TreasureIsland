70e8c38773d3c2c21fac168cd0e4d3d70f29b7e7
mushroomvoodoo <normeeboy@gmail.com>
2023-05-06 11:50:33 +0300

    end...

--- app/src/main/java/com/example/treasureisland/DrLivseyActivity.java
+++ app/src/main/java/com/example/treasureisland/DrLivseyActivity.java
@@ -1,26 +1,43 @@
 package com.example.treasureisland;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.TextView;
 
 
 public class DrLivseyActivity extends AppCompatActivity {
 
-    ImageButton it;
+    ImageButton drLivse;
+    TextView txt, txt1;
+    ConstraintLayout back;
+    boolean clicked = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getSupportActionBar().hide();
         setContentView(R.layout.activity_dr_livsey);
-        it = (ImageButton) findViewById(R.id.it);
-        it.setOnLongClickListener(new View.OnLongClickListener() {
+        drLivse = (ImageButton) findViewById(R.id.it);
+        txt = (TextView) findViewById(R.id.txt);
+        txt1 = (TextView) findViewById(R.id.txt1);
+        back = (ConstraintLayout) findViewById(R.id.back);
+        drLivse.setOnLongClickListener(new View.OnLongClickListener() {
             @Override
             public boolean onLongClick(View v) {
-                startActivity(new Intent(DrLivseyActivity.this, ItActivity.class));
+                if(clicked == false){
+                    drLivse.setBackground(getDrawable(R.drawable.it));
+                    txt1.setText("ОНО");
+                    txt1.setTextColor(getColor(R.color.mhh));
+                    txt.setText("ИНФОРМАЦИЯ ОТСУТСТВУЕТ.\nОНО ХОЧЕТ ЕСТЬ.");
+                    txt.setTextColor(getColor(R.color.mhh));
+                    back.setBackgroundColor(getColor(R.color.hmm));
+                    return clicked = true;
+                }
                 return false;
             }
         });
